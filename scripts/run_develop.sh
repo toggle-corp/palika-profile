1f231b6a0bb1b7cc7ae94b23101694cb679bb5b8
Navin Ayer <ayernavin@gmail.com>
2019-03-01 19:16:14 +0545

    Fix migration issue

--- scripts/run_develop.sh
+++ scripts/run_develop.sh
@@ -3,6 +3,5 @@
 export PYTHONUNBUFFERED=1
 pip3 install -r requirements.txt
 python3 manage.py migrate --no-input
-python3 manage.py createinitialrevisions
 python3 manage.py run_celery_dev &
 python3 manage.py runserver 0.0.0.0:8000
